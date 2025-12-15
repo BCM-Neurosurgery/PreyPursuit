d8f2be82081f27bd8a32c891912f48e96fb9158f
tahaismail424 <taha.ismail424@gmail.com>
2025-12-14 18:24:07 -0600

    working switch identification code

--- src/behav_glm/switch_filter.py
+++ src/behav_glm/switch_filter.py
@@ -3,31 +3,11 @@ import numpy as np
 from scipy.signal import welch
 import pandas as pd
 
-def id_bad_trials(trial_wts: List[np.ndarray], fs=60, nperseg=1024) -> List[bool]:
-    # flag trials whose shift matrix fluctuates too quickly
-    # 95% of signal power should be below 2 Hz
-
-    good_idx = np.zeros(len(trial_wts))
-    for trial, wt in enumerate(trial_wts):
-        f, Pxx = welch(wt.flatten(), fs, nperseg=nperseg)
-        # try to get power percentage
-        try:
-            total_power = np.sum(Pxx)
-            cdf = np.cumsum(Pxx) / total_power
-
-            # ensure cdf value at .95 is less than 2
-            if np.where(cdf > .95)[0][0] < np.where(f > 2)[0][0]:
-                good_idx[trial] = 1
-        except Exception as e:
-            print(e)
-            good_idx[trial] = 0
-    return good_idx
-
 
 def identify_crossings(trial_wts):
     cross_idcs = []
-    for idx, wt in enumerate(trial_wts):
-        y = wt[idx].copy()
+    for wt in trial_wts:
+        y = wt.copy()
         y -= 0.5
 
         # get indices where we cross 0
@@ -37,12 +17,19 @@ def identify_crossings(trial_wts):
 
     return cross_idcs
 
-def filter_crossings(cross_idcs, n_timestamps, start_drop_thresh=20, end_drop_thresh=11, diff_thresh=3):
+def filter_crossings(cross_idcs, trial_wts, start_drop_thresh=20, end_drop_thresh=11, diff_thresh=3):
     filtered_idcs = []
-    for cross_idx in cross_idcs:
+    # id poor trials and skip those while filtering
+    good_idx = _id_bad_trials(trial_wts)
+    for idx, cross_idx in enumerate(cross_idcs):
+        # remove crossings for bad trials
+        if not good_idx[idx]:
+            filtered_idcs.append(np.array([], dtype=int))
+            continue
+        n_timestamps = len(trial_wts[idx])
         # drop crossings too close to start and end
-        start_mask = (cross_idx - start_drop_thresh) <= 0
-        end_mask = (cross_idx + end_drop_thresh) > n_timestamps
+        start_mask = (cross_idx - start_drop_thresh) > 0
+        end_mask = (cross_idx + end_drop_thresh) <= n_timestamps
 
         # also drop indices that are too close together (drop the prior one)
         diff_mask = np.ones_like(cross_idx, dtype=bool)
@@ -83,7 +70,22 @@ def get_cross_windows(trial_wts, cross_idcs, trial_ids, window_size=8):
     }
     return wt_windows, pd.DataFrame(window_info)
 
+def _id_bad_trials(trial_wts: List[np.ndarray], fs=60, nperseg=1024) -> List[bool]:
+    # flag trials whose shift matrix fluctuates too quickly
+    # 95% of signal power should be below 2 Hz
 
+    good_idx = np.zeros(len(trial_wts))
+    for trial, wt in enumerate(trial_wts):
+        f, Pxx = welch(wt.flatten(), fs, nperseg=nperseg)
+        # try to get power percentage
+        try:
+            total_power = np.sum(Pxx)
+            cdf = np.cumsum(Pxx) / total_power
 
-
-
+            # ensure cdf value at .95 is less than 2
+            if np.where(cdf > .95)[0][0] < np.where(f > 2)[0][0]:
+                good_idx[trial] = 1
+        except Exception as e:
+            print(e)
+            good_idx[trial] = 0
+    return good_idx.astype(bool)
--- src/behav_glm/switch_typing.py
+++ src/behav_glm/switch_typing.py
@@ -1,6 +1,8 @@
 from sklearn.decomposition import PCA
 from scipy.stats import pearsonr
 import numpy as np
+import pandas as pd
+import ruptures as rpt
 
 def get_switch_types(wt_windows, window_data, split1_thresh=0.9, split2_thresh=0.7, split3_thresh=0.97):
     # zscore windows
@@ -31,11 +33,12 @@ def get_switch_types(wt_windows, window_data, split1_thresh=0.9, split2_thresh=0
 
     # further split type 1 shifts into type 1 vs type 3
     type1_windows = windows_z[switch_type1s]
-    subscores_all = pca.fit_transform(type1_windows[:, 0].transpose())
+    pca = PCA(n_components = 5)
+    subscores_all = pca.fit_transform(type1_windows.transpose())
     subscores_pc1 = (subscores_all[:, 0] - np.mean(subscores_all[:, 0])) / np.std(subscores_all[:, 0])
     score_corr = [pearsonr(subscores_pc1, crossing)[0] for crossing in type1_windows]
     score_corr = np.abs(np.array(score_corr))
-    switch_type3s = switch_type1s[np.where(score_corr < split2_thresh)[0]]
+    switch_type3s = switch_type1s[np.where(score_corr < split3_thresh)[0]]
     switch_types[switch_type3s] = 3
 
     # get subtypes for type 1 (1 for hi-lo, -1 for lo-hi)
@@ -49,11 +52,74 @@ def get_switch_types(wt_windows, window_data, split1_thresh=0.9, split2_thresh=0
 
     # turn switch types into pandas dataframe with al lreelvant switch typing informat
     switch_data = {
+        **window_data,
         'type': switch_types,
         'subtype': subtypes,
-        'cross_idx': window_data['cross_idx']
     }
     switch_df = pd.DataFrame(switch_data)
+    return switch_df
+
+def detect_switch_bounds(trial_wts, switch_df, window_size=8):
+    switch_starts = []
+    switch_ends = []
+    window_sizes = [15, 10, 5]
+    for _, row in switch_df.iterrows():
+        trial_idx = row.trial_idx
+        trial_wt = trial_wts[trial_idx]
+        # now try to calculate switch bounds with largest possible window
+        try:
+            for window in window_sizes:
+                if row.cross_idx - window > 0:
+                    break
+            if row.cross_idx - window <= 0:
+                raise ValueError('crossing is too near the edge for change-point detection')
+            window_idcs = np.arange(row.cross_idx - window, row.cross_idx + window)
+
+            # now use change-point detection to calculate switch bounds
+            # first with 2nd derivative
+            changepoint_algo = rpt.Binseg(model='rbf')
+            dd_wt = np.gradient(np.gradient(trial_wt[window_idcs].flatten())).reshape(-1, 1)
+            changepoint_algo.fit(dd_wt)
+            bound_idx_rbf = np.array(changepoint_algo.predict(n_bkps=3))
+
+            # now with 1st derivative
+            changepoint_algo = rpt.Binseg(model='l1')
+            d_wt = np.gradient(trial_wt[window_idcs].flatten()).reshape(-1, 1)
+            changepoint_algo.fit(d_wt)
+            bound_idx_l1 = np.array(changepoint_algo.predict(n_bkps=3))
+
+            # use average of two to get estimated bounds
+            average_bounds = ((bound_idx_rbf[[0, 2]] + bound_idx_l1[[0, 2]]) / 2).astype(int)
+
+            # now add vounds
+            switch_starts.append(average_bounds[0])
+            switch_ends.append(average_bounds[1])
+
+        # append nan in case any errors
+        except Exception as e:
+            switch_starts.append(np.nan)
+            switch_ends.append(np.nan)
+
+    switch_df['start_idx'] = switch_starts
+    switch_df['start_idx'] = switch_df['start_idx']
+    switch_df['end_idx'] = switch_ends
+    switch_df['end_idx'] = switch_df['end_idx']
+
+    # now get starts and end in reference to whole trial
+    switch_df['start_idx'] = switch_df['cross_idx'] - switch_df['start_idx']
+    switch_df['end_idx'] = switch_df['cross_idx'] + switch_df['end_idx'] - window_size
+    return switch_df
+
+def add_relative_reward(switch_df, session_df):
+    rel_rewards = []
+    for _, row in switch_df.iterrows():
+        trial_id = row.trial_id
+        trial_row = session_df[session_df['trial_num'] == trial_id].iloc[0]
+        rel_reward = np.abs(trial_row.prey2_val - trial_row.prey1_val)
+        rel_rewards.append(rel_reward)
+    switch_df['relative_reward'] = rel_rewards
+    switch_df['relative_reward'] = switch_df['relative_reward'].astype(int)
+    return switch_df
 
 
 
